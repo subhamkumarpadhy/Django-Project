3f83444a3fef1cf1e7a1990c1c12962394b2e03d
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2025-06-24 21:22:02 +0530

    Made some useful changes

--- threads/threads/settings.py
+++ threads/threads/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(tgm712-^tm)^^!*way7&d^wx%^=l59pj(g!q*j5!kteyb^^@4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
